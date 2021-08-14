37b601e65222fa2808e7a03c652fa2aae8a804fb
micah3252625 <micahmustaham@gmail.com>
2021-08-14 21:07:56 +0800

    updated login.php

--- login.php
+++ login.php
@@ -25,18 +25,11 @@
                                         <div class="form-group">
                                             <input type="text" name="username" class="form-control form-control-user"
                                                 id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Username">
+                                                placeholder="Enter Username" required>
                                         </div>
                                         <div class="form-group">
                                             <input type="password" name="password" class="form-control form-control-user"
-                                                id="exampleInputPassword" placeholder="Password">
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Remember
-                                                    Me</label>
-                                            </div>
+                                                id="exampleInputPassword" placeholder="Password" required>
                                         </div>
                                         <input class="btn btn-primary btn-user btn-block" type="submit" name="submit" value="Login" class="login-btn">
                             
